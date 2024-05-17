da2fef1e625fb14d5268f0aca3a3539e4a1e8f9c
Chandra sekhar vattem <chandu@Chandus-MacBook-Air.local>
2024-05-18 00:29:49 +0530

    adding user profile feature

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
-import express,{Request,Response} from "express"
-import cors from "cors"
-import "dotenv/config"
-import mongoose from "mongoose"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose
-    .connect(process.env.MONGODB_CONNECTION_STRING as string)
-    .then(()=>console.log("connected to database"))
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to database"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const app=express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.use("/health", async (req: Request, res: Response) => {
+  res.send({ message: "Health OK!" });
+});
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
-app.use("/health",async (req:Request,res:Response)=>{
-    res.send({message:"Health OK!"})
-})
-app.use("/api/my/user",myUserRoute);
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello!" });
+});
 
-app.get("/test",async (req:Request,res:Response)=>{
-    res.json({message:"Hello!"})
-})
-
-app.listen(3000,()=>{
-    console.log("Server started on localhost:3000")
-})
+app.listen(3000, () => {
+  console.log("Server started on localhost:3000");
+});
